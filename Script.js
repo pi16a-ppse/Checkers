d1256acf93f5429768e1b6897f5736cb80b6982e
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2019-03-07 22:30:16 +0300

    Код для перемещения шашки по полю

--- Script.js
+++ Script.js
@@ -42,3 +42,40 @@ var tableLimit, reverseTableLimit ,  moveUpLeft,moveUpRight, moveDownLeft,moveDo
 		moveDeviation = 6;
 	}
 /*================объявление класса=========*/
+var square_p = function(square,index){
+	this.id = square;
+	this.ocupied = false;
+	this.pieceId = undefined;
+	this.id.onclick = function() {
+		makeMove(index);
+	}
+}
+var checker = function(piece,color,square) {
+	this.id = piece;
+	this.color = color;
+	this.king = false;
+	this.ocupied_square = square;
+	this.alive = true;
+	this.attack = false;
+	if(square%8){
+		this.coordX= square%8;
+		this.coordY = Math.floor(square/8) + 1 ;
+	}
+	else{
+		this.coordX = 8;
+		this.coordY = square/8 ;
+	}
+	this.id.onclick = function  () {
+		showMoves(piece);	
+	}
+}
+checker.prototype.setCoord = function(X,Y){
+	var x = (this.coordX - 1  ) * moveLength + moveDeviation;
+	var y = (this.coordY - 1 ) * moveLength  + moveDeviation;
+	this.id.style.top = y + 'px';
+	this.id.style.left = x + 'px';
+}
+checker.prototype.changeCoord = function(X,Y){
+	this.coordY +=Y;
+	this.coordX += X;
+}
